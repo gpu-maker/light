0ac83d82dc8bed95a1bfc6ab99232dc103473fca
gpu-maker <32omelada@sandiaprep.org>
2026-02-03 14:44:40 -0700

    Add map locations for travel scenes

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ let state = {
   unlockedMap: ["street"], // 👈 only street at start
   unlockedEndings: []
 };
+const mapLocations = {
+  street: {
+    name: "Street",
+    travelScene: "street_travel"
+  },
+  alley: {
+    name: "Alley",
+    travelScene: "alley_travel"
+  },
+  subway: {
+    name: "Subway",
+    travelScene: "subway_travel"
+  },
+  hospital: {
+    name: "Hospital",
+    travelScene: "hospital_travel"
+  },
+  tower: {
+    name: "Tower",
+    travelScene: "tower_travel"
+  }
+};
